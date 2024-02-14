aefc2cd6f79acf0c43af2a230e37e07f03b821aa
Ron Gilmour <rgilmour70@gmail.com>
2024-02-14 11:41:15 -0500

    Fixed a bug where images were not displaying in dev

--- src/components/ModalThumbnail.jsx
+++ src/components/ModalThumbnail.jsx
@@ -1,69 +1,78 @@
-import React, { Component, Fragment } from 'react';
-import ReactModal from 'react-modal';
+import React, { Component, Fragment } from "react";
+import ReactModal from "react-modal";
 
 const modalStyles = {
-	content : {
-		top: '10%',
-		left: '10%',
-		'min-width': '60%'
-	}
+  content: {
+    top: "10%",
+    left: "10%",
+    "min-width": "60%",
+  },
 };
 
-ReactModal.setAppElement('#root');
+ReactModal.setAppElement("#root");
 
 class ModalThumbnail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      imageUrl: props.imageUrl,
+      alt: props.alt,
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			showModal: false,
-			imageUrl: props.imageUrl,
-			alt: props.alt
-		}
-	}
-	
-	handleOpenModal = () => {
-		this.setState({ showModal: true });
-	};
-	  
-	handleCloseModal = () => {
-		this.setState({ showModal: false });
-	};
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
 
-	render() {
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
 
-		if (this.state.imageUrl) {
-			const parts = this.state.imageUrl.split(/\//);
-			let thumbUrl = '';
-			for (let i=0; i<parts.length - 1; i++) {
-			    thumbUrl += parts[i] + '/';
-			}
-			thumbUrl += 'thumbnails/' + parts.pop();
-			return (
-				<Fragment>
-					<div className="thumbnail-container" onClick={this.handleOpenModal}>
-						<img
-							src={thumbUrl}
-							alt={this.state.alt}
-						/>
-					</div>
-					<ReactModal 
-						isOpen={this.state.showModal}
-						shouldCloseOnOverlayClick={true}
-						onRequestClose={this.handleCloseModal}
-						style={modalStyles}
-					>
-						<div className="modal-header">
-							<button onClick={this.handleCloseModal} type="button" className="close">&times;</button>
-						</div>
-						<div className="modal-body"><img src={this.state.imageUrl} alt={this.state.alt} /></div>
-					</ReactModal>
-				</Fragment>
-			)
-		} else {
-			return null;
-		}
-	}
+  render() {
+    if (this.state.imageUrl) {
+      const parts = this.state.imageUrl.split(/\//);
+      let thumbUrl = "";
+      for (let i = 0; i < parts.length - 1; i++) {
+        thumbUrl += parts[i] + "/";
+      }
+      thumbUrl += "thumbnails/" + parts.pop();
+      return (
+        <Fragment>
+          <div className="thumbnail-container" onClick={this.handleOpenModal}>
+            <img
+              src={process.env.PUBLIC_URL + "/" + thumbUrl}
+              alt={this.state.alt}
+            />
+          </div>
+          <ReactModal
+            isOpen={this.state.showModal}
+            shouldCloseOnOverlayClick={true}
+            onRequestClose={this.handleCloseModal}
+            style={modalStyles}
+          >
+            <div className="modal-header">
+              <button
+                onClick={this.handleCloseModal}
+                type="button"
+                className="close"
+              >
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <img
+                src={process.env.PUBLIC_URL + "/" + this.state.imageUrl}
+                alt={this.state.alt}
+              />
+            </div>
+          </ReactModal>
+        </Fragment>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default ModalThumbnail;
